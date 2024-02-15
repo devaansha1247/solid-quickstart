2e98050400258f5bd2d358e2e19fdfa9eea0c859
devaansha1247 <144129419+devaansha1247@users.noreply.github.com>
2024-02-15 21:54:02 +0530

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -22,17 +22,8 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
+      <Body> 
+        
       </Body>
     </Html>
   );
